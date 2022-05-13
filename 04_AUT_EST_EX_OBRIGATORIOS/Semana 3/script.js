5e41c215da7551b0cc5a95bb3d759061646dc606
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-13 02:07:47 -0300

    Atualização do exercício só, faz tempo isso e nem comitei

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/script.js
@@ -1,18 +1,14 @@
 /* DECLARAÇÃO DE VARIÁVEIS */
-
-var gravidade = 10;
+const gravidade = 10;
 var calcResDiv = "calc";
-
-
-//
+// 
 function calcular() {
     velocidadeInicial = Number(document.getElementById("velocidadeInicial").value);
     var alturaMaxima = (velocidadeInicial ** 2) / (2 * gravidade);
     var tempoSubida = (velocidadeInicial) / (gravidade);
     ShowResult(alturaMaxima, tempoSubida, velocidadeInicial);
 };
-
 // Função que mostra a altura máxima obtida
 function ShowResult(h, t, v) {
-    document.getElementById(calcResDiv).innerHTML += `<br /> Lançando verticalmente um objeto com velocidade incial de ${v} metros por segundo, a <strong>Altura Máxima</strong> que o objeto alcança é de ${h} metros e demora ${t} segundos para subir até lá. <br /> `;
+    document.getElementById(calcResDiv).innerHTML = `<br /> Lançando verticalmente um objeto com velocidade incial de ${v} metros por segundo, a <strong>Altura Máxima</strong> que o objeto alcança é de ${h} metros e demora ${t} segundos para subir até lá. <br /> `;
 };
