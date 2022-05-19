5521e2a8e9c5a6bdf3e6e20a965dca59e6f055bb
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-19 09:28:13 -0300

    Exercício 1 completo

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercício 1/script.js
@@ -0,0 +1,66 @@
+﻿/* 
+=======================
+Declaração de variáveis
+=======================
+*/
+
+var calcResDiv = "#calc";
+
+/* 
+=======================
+Funções
+======================= 
+*/
+
+/* Função que calcula uma soma e mostra a operação realizada e o resultado obtido */
+function Calculate() {
+    var select = document.getElementById('operador');
+    var op = select.options[select.selectedIndex].value;
+    // $('#operador :selected').text();
+
+
+    var x = Number(document.getElementById('valor1').value);
+    var y = Number(document.getElementById('valor2').value);
+
+    var result = Calc(x, y, op);
+    ShowOp(x, y, op);
+    ShowResult(result);
+};
+
+
+/* Função que calcula uma operação entre dois números recebidos como parâmetro */
+function Calc(x, y, op) {
+    var result;
+    if (op == "+") {
+        result = x + y;
+    };
+    if (op == "*") {
+        result = x * y;
+    };
+    if (op == "^") {
+        result = x ** y;
+    };
+    if (op == "/") {
+        result = x / y;
+    };
+    if (op == "%") {
+        result = x % y;
+    };
+    if (op == "-") {
+        result = x - y;
+    };
+    return result;
+}
+
+
+/* Função que mostra a operação realizada */
+function ShowOp(x, y, op) {
+    $(calcResDiv).append(`<br />${x} ${op} ${y} = `);
+    // Alternativa: .. += "<br />" + x + " " + op + " " + y + " = "
+}
+
+
+/* Função que mostra o resultado obtido */
+function ShowResult(res) {
+    $(calcResDiv).append(res);
+}
