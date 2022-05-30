f3e3ed0d4ab46e545656bb6e260aff1f4be5bbbe
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-30 17:13:49 -0300

    Update exercício 1 e Exercício 2 completo

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 2/script.js
@@ -0,0 +1,15 @@
+﻿/* 
+=======================
+Funções
+======================= 
+*/
+
+// Função que troca os valores
+function Troca() {
+
+    var valor1 = document.getElementById('valor1').value;
+    var valor2 = document.getElementById('valor2').value;
+
+    document.getElementById('valor1').value = valor2;
+    document.getElementById('valor2').value = valor1;
+};
