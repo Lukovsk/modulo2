a60f65a62f5f7e39ea88e860bb4411021d396e29
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-30 18:44:07 -0300

    Exercício 3 INCOMPLETO, não consegui fazer a parte da verificação do valor para notificar o usuário

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 3/script.js
@@ -0,0 +1,24 @@
+﻿/* 
+=======================
+Funções
+======================= 
+*/
+
+function mascara(i) {
+
+
+    var v = i.value;
+
+    // if (isNaN(v[v.length - 1])) { // impede entrar outro caractere que não seja número
+    //     i.value = v.substring(0, v.length - 1);
+    //     return;
+    // }
+
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3) {
+        i.value += ") ";
+    };
+    if (v.length == 9) {
+        i.value += "-";
+    }
+}
