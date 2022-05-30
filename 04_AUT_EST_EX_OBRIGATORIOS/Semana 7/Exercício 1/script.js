23bcb44a86b6c1d606acc4993620a221015e4327
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-30 16:59:21 -0300

    Exercício 1 completo

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 1/script.js
@@ -0,0 +1,34 @@
+﻿/* 
+=======================
+Funções
+======================= 
+*/
+
+// Função que recebe o valor da quantidade, o operador como argumento do botão, calcula o resultado da soma/subtração e, no fim, define o resultado como valor do input
+function Calculate(operador) {
+
+    var x = Number(document.getElementById('quantidade').value);
+    var op = operador;
+    var result = Calc(x, op);
+    // console.log(op);
+
+    ShowResult(result);
+};
+
+/* Função que soma ou subtrai do valor que tá no input e retorna o resultado dessa operação */
+function Calc(x, op) {
+    var result;
+    if (op == "+") {
+        result = x + 1;
+    };
+    if (op == "-") {
+        result = x - 1;
+    };
+    return result;
+}
+
+/* Função que mostra o resultado obtido */
+function ShowResult(res) {
+    console.log(res);
+    document.getElementById('quantidade').value = res;
+}
