2413d435431a83f59e3116d1852b774d03b7a56c
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-31 01:27:06 -0300

    80% do Exercício 5, falta descobrir como fazer um array com as notas e descobrir quais as maiores e menores notas

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 5/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 5/script.js
@@ -10,14 +10,48 @@ function CriarAlunos() {
     quantidadeAlunos = Number(document.getElementById('quantidadeAlunos').value);
     let i = 0;
     while (i < quantidadeAlunos) {
-        document.getElementById("listaAlunos").innerHTML += `<br /> <label> ${i}: <input> </label> <input type="number" id="notaProva" placeholder="Prova"> <input type="number" id="notaTrabalho" placeholder="Trabalho">`;
+        document.getElementById("listaAlunos").innerHTML += `<br /> <label> Aluno ${i + 1}: </label> <input type="number" id="notaProva${i + 1}" placeholder="Prova"> <input type="number" id="notaTrabalho${i + 1}" placeholder="Trabalho">`;
         i++
     }
-    document.getElementById("listaAlunos").innerHTML += `<br /> <button onclick="Calculate()" class="button-calc"> Mostrar Médias </button>`;
+    document.getElementById("listaAlunos").innerHTML += `<br /> <br /> <button onclick="Calculate()" class="button-calc"> Mostrar Médias </button>`;
 }
-// Função que recebe o valor da quantidade, o operador como argumento do botão, calcula o resultado da soma/subtração e, no fim, define o resultado como valor do input
-function Calculate() {
-    
 
-    document.getElementById("calc").innerHTML = `<br /> Com essa quantidade de passageiros, a receita da viagem será de ${preço} reais no total.`;
+// a menor e a maior notas de prova;
+// a menor e a maior notas de trabalho.
+
+function Calculate() {
+    debugger
+    // média de cada aluno:
+    let k = 0;
+    var numeradorMedia = 0;
+    var numeradorMediaProvas = 0;
+    var numeradorMediaTrabalhos = 0;
+    while (k < quantidadeAlunos) {
+        idnotaProva = String("notaProva" + String(k + 1));
+        prova = Number(document.getElementById(idnotaProva).value);
+        idnotaTrabalho = String("notaTrabalho" + String(k + 1));
+        trabalho = Number(document.getElementById(idnotaTrabalho).value);
+
+        media = ((2 * prova + 3 * trabalho) / 5);
+        numeradorMedia += media;
+        numeradorMediaProvas += prova;
+        numeradorMediaTrabalhos += trabalho;
+
+        document.getElementById("listaMédias").innerHTML += `<br /> <label> Aluno ${k + 1}: Média <span id="media${k + 1}"> ${media} </span> </label>`
+
+        k++
+    }
+
+    // média geral (considerando todos os alunos)
+    mediaGeral = numeradorMedia / (k + 1);
+    document.getElementById("outrasMédias").innerHTML += `<br /> A média geral da turma foi ${mediaGeral}`;
+
+    // a média artiméticas das notas de prova;
+    mediaProvas = numeradorMediaProvas / (k + 1);
+    document.getElementById("outrasMédias").innerHTML += `<br /> A média das notas das provas foi ${mediaProvas}`;
+
+    // a média artiméticas das notas de trabalho;
+    mediaTrabalho = numeradorMediaTrabalhos / (k + 1);
+    document.getElementById("outrasMédias").innerHTML += `<br /> A média das notas dos trabalhos foi ${mediaTrabalho}`;
+
 };
