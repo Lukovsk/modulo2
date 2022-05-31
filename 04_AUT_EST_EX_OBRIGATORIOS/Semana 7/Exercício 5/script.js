2f1e883f95e77b6c4e87e912aea3f73da17309c1
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-30 22:43:48 -0300

    Parte do exercício 5

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 5/script.js
@@ -0,0 +1,23 @@
+﻿/* 
+=======================
+Funções
+======================= 
+*/
+var calc = "#calc";
+
+function CriarAlunos() {
+    debugger
+    quantidadeAlunos = Number(document.getElementById('quantidadeAlunos').value);
+    let i = 0;
+    while (i < quantidadeAlunos) {
+        document.getElementById("listaAlunos").innerHTML += `<br /> <label> ${i}: <input> </label> <input type="number" id="notaProva" placeholder="Prova"> <input type="number" id="notaTrabalho" placeholder="Trabalho">`;
+        i++
+    }
+    document.getElementById("listaAlunos").innerHTML += `<br /> <button onclick="Calculate()" class="button-calc"> Mostrar Médias </button>`;
+}
+// Função que recebe o valor da quantidade, o operador como argumento do botão, calcula o resultado da soma/subtração e, no fim, define o resultado como valor do input
+function Calculate() {
+    
+
+    document.getElementById("calc").innerHTML = `<br /> Com essa quantidade de passageiros, a receita da viagem será de ${preço} reais no total.`;
+};
