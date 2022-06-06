e766a2975b4389574324dfecede6b8ac566a9a16
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-06-06 19:21:19 -0300

    Exercício 1 semana 9

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 1/script.js
@@ -0,0 +1,23 @@
+/* 
+=======================
+Funções
+======================= 
+*/
+
+// Função que é chamada pelo botão e pega a centena do valor a partir de uma divisão por 100 e seu valor piso. Depois ela diz se esse valor é par ou ímpar
+function Calculate() {
+    debugger
+
+    valor = document.getElementById('valor').value;
+
+    var centena = Math.floor(valor / 100);
+
+    var resultado;
+    if (centena % 2 == 0) {
+        resultado = "par";
+    } else {
+        resultado = "ímpar";
+    };
+
+    document.getElementById('calc').innerHTML = "<h3> A centena desse valor é " + resultado + ". </h3>";
+};
