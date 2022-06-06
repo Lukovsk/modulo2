2a8c9e2577e7eba4d1bde512aa1c86cf8c999750
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-06-05 22:22:56 -0300

    update exercício 1 (diminui o código à bessa)

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 1/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 1/script.js
@@ -8,27 +8,15 @@ Funções
 function Calculate(operador) {
 
     var x = Number(document.getElementById('quantidade').value);
-    var op = operador;
-    var result = Calc(x, op);
-    // console.log(op);
-
-    ShowResult(result);
-};
-
-/* Função que soma ou subtrai do valor que tá no input e retorna o resultado dessa operação */
-function Calc(x, op) {
-    var result;
-    if (op == "+") {
+    // Aqui é escolhido o que fará com o resultado (acrescer 1 ou diminuir 1) a depender do argumento da função.
+    if (operador == "+") {
         result = x + 1;
     };
-    if (op == "-") {
+    if (operador == "-") {
         result = x - 1;
     };
-    return result;
-}
 
-/* Função que mostra o resultado obtido */
-function ShowResult(res) {
-    console.log(res);
-    document.getElementById('quantidade').value = res;
-}
+    document.getElementById('quantidade').value = result; // mostra o resultado
+};
+
+
