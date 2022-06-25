e07f89530ee110505142b3c3e1d6aa9de2e66650
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-06-25 13:29:08 -0300

    Comentários exercício 2

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 2/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 2/script.js
@@ -10,11 +10,12 @@ function Calculate() {
     // objetivo: formar uma lista com cada algarismo do valor e depois somar cada item da lista
     // formar uma lista com cada algarismo do valor:
     var i = 0
-    var lista = Array.from(valor);
+    var lista = Array.from(valor); // constrói um array com todos os algarismos de "valor"
+    // ex.: se valor = 1234, então Arra.from(valor) retorna ["1", "2", "3", "4"] como string mesmo
     console.log(lista);
     var resultado = 0;
     while (i < lista.length) {
-        resultado += Number(lista[i]);
+        resultado += Number(lista[i]); // depois, adiciona ao resultado todos os valores sem o formato string
         // console.log(resultado + " and " + lista[i])
         i++
     };
