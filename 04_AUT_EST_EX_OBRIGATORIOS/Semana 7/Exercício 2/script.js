9e663941284f764a32a378955c53ba657707b783
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-06-25 13:35:17 -0300

    Fui lerdo, era pra ter comentado na semana 7 KKKKK, enfim, comentários do exercício 2

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 2/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercício 2/script.js
@@ -7,9 +7,9 @@ Funções
 // Função que troca os valores
 function Troca() {
 
-    var valor1 = document.getElementById('valor1').value;
-    var valor2 = document.getElementById('valor2').value;
+    var valor1 = document.getElementById('valor1').value; // armazena o que estiver no primeiro input
+    var valor2 = document.getElementById('valor2').value; // armazena o que estiver no segundo input
 
-    document.getElementById('valor1').value = valor2;
-    document.getElementById('valor2').value = valor1;
+    document.getElementById('valor1').value = valor2; // o que estava no primeiro input vira o que estava no segundo input
+    document.getElementById('valor2').value = valor1; // o que estava no segundo input vira oq ue estava no primeiro input
 };
