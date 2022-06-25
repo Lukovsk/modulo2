6af66ae59ba5e3e29d9092a6f72646124d39c0e9
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-06-25 13:33:01 -0300

    Comentários exercício 4

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 4/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 4/script.js
@@ -13,23 +13,26 @@ function Calculate() {
     // calcula a série de fibonacci
     // na verdade, calcula cada número e já adiciona na página
     var i = 0;
+    // sequência de fibonacci começa com dois 1's
     valor1 = 1;
     valor2 = 1;
-    fibonacci = [];
+    fibonacci = [1, 1];
     while (i < valor) {
-        var novoValor = valor1 + valor2;
-        valor1 = valor2;
-        valor2 = novoValor;
-        fibonacci.push(novoValor)
+        var novoValor = valor1 + valor2; // armazena a soma dos dois valores anteriores
+        valor1 = valor2; // o primeiro valor se torna o segundo
+        valor2 = novoValor; // o segundo valor se torna a soma dos dois primeiros e aquele primeiro valor é perdido (ele ainda está armazenado no array fibonacci[])
+        fibonacci.push(novoValor) // adiciona a soma dos dois
         // console.log(fibonacci)
         i++
     }
 
+    // aqui, apenas estou printando na tela todos os valores do array fibonacci[] que são menores que um determinado valor
     var tx = '';
     var j = 0;
-    while (j < valor) {
-        tx += `<p  class="listaBox"> ${fibonacci[j]} </p>`;
+    while (j < valor) { // enquanto o valor teto que a pessoa quer que sejam gerados os fibonacci é menor que j na estrutura do loop
+        tx += `<p  class="listaBox"> ${fibonacci[j]} </p>`; // adiciona na tela
         j++
     }
+    // quando todos os valores até o valor que a pessoa quer forem adicionados, gera esse texto
     document.getElementById('calc').innerHTML = tx;
 };
